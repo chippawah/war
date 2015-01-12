91d9dc9823c4515158b07b773aa86c6447029ef8
Chip Cantrell <charles.cantrell@me.com>
2015-01-12 15:29:59 -0700

    Finished War

--- script.js
+++ script.js
@@ -44,16 +44,41 @@ $(document).ready(function() {
 	}
 	
 	//Now call the shuffle function and save the result of what shuffle returns into your deck variable
-	
+    deck = shuffle(deck);
 	var cards_player_1 = [];
 	var cards_player_2 = [];
 	// write a function called deal that will evently divide the deck up between the two players
-	
+	var deal = function(arr) {
+
+		for (var i = 0; i < arr.length; i++) {
+
+			if (i % 2 === 0) {
+
+				cards_player_1.push(arr[i]);
+
+			} else {
+
+				cards_player_2.push(arr[i]);
+			}
+		};
+	}
 	
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	var war = function(){
+	var war = function(card1, card2){
 	
-		
+		if (card1.number > card2.number) {
+
+			return card1;
+
+		} else if (card1.number < card2.number) {
+
+			return card2;
+
+		} else {
+
+			return false;
+
+		};
 	}
 	
 	var advance = function(){
@@ -61,6 +86,7 @@ $(document).ready(function() {
 		if (cards_player_1.length) {
 			var card_1 = cards_player_1[0];
 			var card_2 = cards_player_2[0];
+			console.log(card_1, card_2)
 			$("#opp-card").html(convert_value_to_string(card_1.number)+" "+card_1.suit);
 			$("#opp-card-count").html(cards_player_1.length);
 			$("#my-card").html(convert_value_to_string(card_2.number)+" "+card_2.suit);
@@ -73,7 +99,48 @@ $(document).ready(function() {
 	//create a play function
 		//compare the cards
 		//give the winner both cards (at end of deck)
+	
+	deal(deck);	
+
 	var play = function(){
+
+
+		var winner = war(cards_player_1[0], cards_player_2[0]);
+
+		if (winner) {
+
+			if (winner === cards_player_1[0]) {
+
+				cards_player_1.push(cards_player_1.splice(0, 1)[0]);
+				cards_player_1.push(cards_player_2.splice(0, 1)[0]);
+
+			} else {
+
+				cards_player_2.push(cards_player_1.splice(0, 1)[0]);
+				cards_player_2.push(cards_player_2.splice(0, 1)[0]);
+
+			}
+
+		} else {
+
+			var tieWinner = war(cards_player_1[3], cards_player_2[3]);
+
+			if (tieWinner) {
+
+				if (tieWinner === cards_player_1[3]) {
+
+					cards_player_1.concat(cards_player_1.splice(0, 4));
+					cards_player_1.concat(cards_player_2.splice(0, 4));
+
+				} else {
+
+					cards_player_2.concat(cards_player_1.splice(0, 4));
+					cards_player_2.concat(cards_player_2.splice(0, 4));
+
+				};
+
+			}
+		}
 		
 		//this function (defined below) will continue to the next turn
 		advance();
